3de3e1d2be6da0768953c10db584fcfbc3c0bbaa
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-17 13:26:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 def my_collect(languages)
   if block_given?
   i = 0 
-
+  
   while i < languages.length
   languages[i] = yield(languages[i])
   i = i + 1
 end
-new_lan
+languages
 
 else
   puts "Hey! No block was given!"
